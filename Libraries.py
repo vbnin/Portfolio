b79a4ce7f9b2504259a4df3b00ca57433f9699bd
Vincent <31508339+vbnin@users.noreply.github.com>
2017-09-04 18:37:10 +0200

    tests

---
+++ Libraries.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+def ztest():
+    logger.info("Hello logging!")
+    
--- Sandbox.py
+++ Sandbox.py
@@ -7,19 +7,13 @@ This is a quick sandbox used for testing parts of codes
 
 import re
 import logging
+from Libraries import ztest
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s : %(message)s')
+logger = logging.getLogger(__name__)
 
 foo = "inutile = bar"
 IPAddr = "1.2.3.4"
-
-def log(level, msg):
-    logging.basicConfig(level=logging.INFO, format='%(asctime)s : %(message)s')
-    logger = logging.getLogger(__name__)
-    if level == "debug":
-        logger.debug(msg)
-    elif level == "info":
-        logger.info(msg)
-    else:
-        logger.error(msg)
 
 def HpaInfo(Nb, IPAddr, foo):
     Infos = {'Modele':foo,
@@ -28,13 +22,14 @@ def HpaInfo(Nb, IPAddr, foo):
              'Total System Hours':foo,
              'Total Transmit Hours':foo,
              }
-    log("info", "*** Spécifications du HPA #" + Nb + " ***")
-    log("info", "Adresse IP : " + IPAddr)
+    logger.info("*** Spécifications du HPA #" + Nb + " ***")
+    logger.info("Adresse IP : " + IPAddr)
     for key in Infos:
         m = re.search('(.*)\ =\ (.*)', Infos[key])
         if m is not None:
-            log("info", key + " : " + m.group(2))
-    log("info", "***")
+            logger.info(key + " : " + m.group(2))
+    logger.info("***")
 
+ztest()
 HpaInfo('1', IPAddr, foo)
 HpaInfo('2', IPAddr, foo)
